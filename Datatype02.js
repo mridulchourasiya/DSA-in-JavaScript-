31103d20ef697dcb52a382d5acf75fc67f9e1b90
Mridul chourasia <99160348+mridulchourasiya@users.noreply.github.com>
2023-12-05 18:25:52 +0530

    Day_01

---
+++ Datatype02.js
@@ -0,0 +1,14 @@
+"use strict" // treate all JS code as newer version
+
+let name = "mridul"
+let age = 18
+let isLoggedin = false
+console.table([name,age,isLoggedin])
+// number => 2 to power 53
+// null  
+// undefined
+
+/* 
+console.log(typeof undefined ) is undefine
+console.log(typeof null ) is object
+*/
--- cosnt_let.js
+++ cosnt_let.js
@@ -7,16 +7,17 @@ const accountID = 144553;
 let accountEmail = "mridulchourasia@gmail.com";
 var accountPassword = "12345";
 accountCity = "jaipur";
-
+let accountState 
 // accountID = 2 not allowed
 // accountEmail = "ms@gmal.com";
 // accountPassword = "22122121";
 // accountCity = "Bangaluru";
 
 console.log(accountID);
-console.table([accountID, accountEmail, accountPassword, accountCity]);
+console.table([accountID, accountEmail, accountPassword, accountCity, accountState]);
 
 
 /*
 Prefer not to use war because of issue in block and scope
 */
+
