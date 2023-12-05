0f3deed453ca0b65ea1ee958e5d82adf526daafe
Mridul chourasia <99160348+mridulchourasiya@users.noreply.github.com>
2023-12-05 18:06:56 +0530

    frist_day01

---
+++ day01.js
@@ -0,0 +1,22 @@
+// console.log("mridul")
+// day 01 in js Varialble and cosnttent
+
+// assign into memeory
+
+const accountID = 144553;
+let accountEmail = "mridulchourasia@gmail.com";
+var accountPassword = "12345";
+accountCity = "jaipur";
+
+// accountID = 2 not allowed
+// accountEmail = "ms@gmal.com";
+// accountPassword = "22122121";
+// accountCity = "Bangaluru";
+
+console.log(accountID);
+console.table([accountID, accountEmail, accountPassword, accountCity]);
+
+
+/*
+Prefer not to use war because of issue in block and scope
+*/
