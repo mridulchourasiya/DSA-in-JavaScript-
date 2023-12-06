5199e65658c683f7ec16ee9b82dbeff37aaa8223
Mridul chourasia <99160348+mridulchourasiya@users.noreply.github.com>
2023-12-06 17:03:21 +0530

    second

---
+++ Day02/conservationOperatior.js
@@ -0,0 +1,27 @@
+// let score =
+
+// console.log(typeof score)
+// let valueInNumber = Number(score)
+// console.log(typeof valueInNumber)
+
+// "33" => 33
+// "33abc" => NaN
+// true => 1 , false => 0
+
+// let isLoggedin = "null"
+// let booleanIsLoggedIn = Boolean(isLoggedin)
+// console.log(booleanIsLoggedIn);
+
+// 1 => true , 0 => false
+// "" => false
+// "hitesh" => true
+
+// *********************Opration****************
+
+let value = 3;
+let negValue = -value;
+
+// console.log(negValue);
+
+
+
---
+++ Day02/datatype-summery.js
@@ -0,0 +1,7 @@
+// Primitive
+
+// 7 type : String, Number, Boolean, Null, undefined, Symbol, BigInt
+// JavaScript is a dynamically typed language,
+// which means that data types of variables are determined by the value they hold at runtime and can change throughout the program as we assign different values to them.
+// Reference type (non primitive)
+// Array, object, Function
